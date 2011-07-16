1b4f25bef3aeff455ecaa3c1a706bae296ad0ccd
Vivek Prakash <vivekprakash@acm.org>
2011-07-16 01:35:13 +0000

    Initial commit of nbsd_libedit and nbsd_libterminfo

---
+++ lib/nbsd_libedit/fcns.c
@@ -0,0 +1,7 @@
+/* Automatically generated file, do not edit */
+#include "config.h"
+#include "el.h"
+private const el_func_t el_func[] = {
+};
+
+protected const el_func_t* func__get() { return el_func; }
