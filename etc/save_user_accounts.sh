6fccb75c9668f30243850ecc3dff6d6319222480
Vivek Prakash <vivekprakash21@gmail.com>
2011-08-03 14:44:31 +0000

    Add script to fix existing accounts' clobbering

--- etc/save_user_accounts.sh
+++ etc/save_user_accounts.sh
@@ -6,29 +6,29 @@ then	echo "Usage: $0 passwd-file"
 	exit 1
 fi
 
-nbsd_passwd="master.passwd"
+NBSD_PASSWD=etc/master.passwd
 
 cat "$1" | while read line
-do 
+do
 	NF="`echo $line | awk -F ':' '{ print NF}'`"
 	if [ $NF = 7 ]
-	then 
+	then
 		user="`echo $line | awk -F ':' '{ print $1 }'`"
 		flag=0
 
-		cat $nbsd_passwd | while read pw_line
+		while read pw_line
 		do
 			pw_name="`echo $pw_line | awk -F ':' '{ print $1 }'`"
 			if [ $user = $pw_name ]
 			then
-				$flag=1
+				flag=1
 				break
 			else
 				continue
 			fi
-		done
-	
-		if [ $flag = 0 ]
+		done<$NBSD_PASSWD
+
+		if [ $flag -eq 0 ]
 		then
 			password="`echo $line | awk -F ':' '{ print $2 }'`"
 			uid="`echo $line | awk -F ':' '{ print $3 }'`"
@@ -36,12 +36,16 @@ do
 			info="`echo $line | awk -F ':' '{ print $5 }'`"
 			homedir="`echo $line | awk -F ':' '{ print $6 }'`"
 			shell="`echo $line | awk -F ':' '{ print $7 }'`"
-		#	`echo $user:$password:$uid:$gid:$info:$homedir:$shell >> master.passwd`
-			echo $user:$password:$uid:$gid:$info:$homedir:$shell
+			echo Setting empty password for $user
+			echo $user::$uid:$gid::0:0:$info:$homedir:$shell >> $NBSD_PASSWD
 		fi
-
 	else
 		echo corrupted passwd file : $1
 		exit 1
 	fi
 done
+
+echo Updated the master.passwd file with existing user accounts
+echo Now run \'make -C etc installpasswd\' to switch to new password format
+
+
